e3be3751d5b0d3e2e828c9a09545384b376dc1c0
Francisco Burgoa <franciscojburgoa@gmail.com>
2023-01-14 19:38:43 -0300

    router api

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -1,4 +1,17 @@
-export default function (req, res) {
-  const miNombre: string = "Francisco";
-  res.send(miNombre);
+import methods from "micro-method-router";
+
+module.exports = methods({
+  async get(req, res) {
+    return getF(req, res);
+  },
+  async patch(req, res) {
+    return patchF(req, res);
+  },
+});
+
+function getF(req, res) {
+  res.status(200).send("Status: OK");
+}
+function patchF(req, res) {
+  res.status(200).send("Status: OK");
 }
---
+++ pages/api/search/index.ts
@@ -0,0 +1,11 @@
+import methods from "micro-method-router";
+
+module.exports = methods({
+  async get(req, res) {
+    return handler(req, res);
+  },
+});
+
+function handler(req, res) {
+  res.status(200).send("Status: OK");
+}
