1aebacd09a337be1888b5de70b4f9fc6d5835703
Francisco Burgoa <franciscojburgoa@gmail.com>
2023-01-14 20:24:38 -0300

    modificación en post order

--- pages/api/order/index.ts
+++ pages/api/order/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import methods from "micro-method-router";
 
 export default methods({
-  async order(req: NextApiRequest, res: NextApiResponse) {
+  async post(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).send("ok");
   },
 });
