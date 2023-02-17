5227748a5b1a7d35e2d3a7ef701d2790e304cab8
Francisco Burgoa <franciscojburgoa@gmail.com>
2023-02-17 17:56:41 -0300

    agregue ruta thanks del front

--- controllers/orders.ts
+++ controllers/orders.ts
@@ -42,8 +42,7 @@ export async function createOrder(
     ],
 
     back_urls: {
-      success:
-        "https://e-commerce-front-jmsfc84e4-francisco-b07.vercel.app/thanks",
+      success: "https://e-commerce-front-six.vercel.app//thanks",
     },
     external_reference: order.id,
     notification_url:
